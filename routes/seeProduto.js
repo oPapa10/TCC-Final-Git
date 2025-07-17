fbf45d5272024353e2f99b106ae13a6b328a4c7e
Ruan Borges da Silveira <borgesruan051@gmail.com>
2025-07-17 11:54:06 -0300

    v2.9

--- routes/seeProduto.js
+++ routes/seeProduto.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const Produto = require('../models/produtoModel');
 
 //Rota para home
 router.get('/', (req, res) => {
-  res.render('seeProduto');
+    // Supondo que Produto é seu model
+    Produto.findAll((err, produtos) => {
+        if (err) return res.status(500).send('Erro ao buscar produtos');
+        res.render('seeProduto', { produtos }); // <-- produtos deve ser passado aqui!
+    });
 });
 
 module.exports = router;
